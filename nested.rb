2c20a4fbd7ae88fdce415d6dc832bc7b94b55cd4
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-02 02:40:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,7 +94,7 @@ def changing_alan
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
+          :known_for => "GUI",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
@@ -109,7 +109,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash   
 end
 
 def adding_to_dennis
